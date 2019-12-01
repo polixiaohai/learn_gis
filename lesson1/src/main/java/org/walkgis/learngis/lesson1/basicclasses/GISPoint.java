f91493c6ec3b9e922da3794d21614cda14b9aa95
Administrator <xx>
2019-12-01 12:55:39 +0800

    第一课

--- lesson1/src/main/java/org/walkgis/learngis/lesson1/basicclasses/GISPoint.java
+++ lesson1/src/main/java/org/walkgis/learngis/lesson1/basicclasses/GISPoint.java
@@ -1,5 +1,28 @@
 package org.walkgis.learngis.lesson1.basicclasses;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+import javafx.scene.text.Font;
+
 public class GISPoint {
     public GISVertex location;
+    public String attribute;
+
+    public GISPoint(GISVertex location, String attribute) {
+        this.location = location;
+        this.attribute = attribute;
+    }
+
+    public void drawPoint(GraphicsContext graphicsContext) {
+        graphicsContext.setFill(Color.RED);
+        graphicsContext.fillArc(location.x, location.y, 3, 3, 0, 360, ArcType.OPEN);
+    }
+
+    public void drawAttribute(GraphicsContext graphicsContext) {
+        graphicsContext.setFont(new Font("宋体", 20));
+        graphicsContext.setFill(Color.BLUE);
+
+        graphicsContext.fillText(attribute, (int) location.x, (int) location.y);
+    }
 }
--- lesson1/src/main/java/org/walkgis/learngis/lesson1/controller/MainController.java
+++ lesson1/src/main/java/org/walkgis/learngis/lesson1/controller/MainController.java
@@ -1,16 +1,76 @@
 package org.walkgis.learngis.lesson1.controller;
 
 import de.felixroske.jfxsupport.FXMLController;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import org.walkgis.learngis.lesson1.basicclasses.GISPoint;
+import org.walkgis.learngis.lesson1.basicclasses.GISVertex;
+
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 
 @FXMLController
 public class MainController implements Initializable {
 
+    @FXML
+    private AnchorPane mainPane;
+    @FXML
+    private Canvas mainCanvas;
+    @FXML
+    private TextField txtX, txtY, txtAttribute;
+    @FXML
+    private Button btnDraw;
+
+    private List<GISPoint> pointList = new ArrayList<>();
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        btnDraw.setOnMouseClicked(this::drawPoint);
+        mainCanvas.setOnMouseClicked(this::canvasClick);
     }
 
+    @FXML
+    private void canvasClick(MouseEvent event) {
+        GISVertex gisVertex = new GISVertex(event.getX(), event.getY());
+        double minDis = Double.MAX_VALUE;
+        int findId = -1;
+        for (int i = 0; i < pointList.size(); i++) {
+            double dis = pointList.get(i).location.distance(gisVertex);
+
+            if (dis < minDis) {
+                minDis = dis;
+                findId = i;
+            }
+        }
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        if (minDis > 5 || findId == -1)
+            alert.setContentText("没有点实体或者点击位置不准确");
+        else
+            alert.setContentText(pointList.get(findId).attribute);
+
+        alert.showAndWait();
+    }
+
+    @FXML
+    private void drawPoint(MouseEvent mouseEvent) {
+        double x = Double.parseDouble(txtX.getText());
+        double y = Double.parseDouble(txtY.getText());
+        String attribute = txtAttribute.getText();
+
+        GISVertex gisVertex = new GISVertex(x, y);
+        GISPoint gisPoint = new GISPoint(gisVertex, attribute);
+        gisPoint.drawPoint(mainCanvas.getGraphicsContext2D());
+        gisPoint.drawAttribute(mainCanvas.getGraphicsContext2D());
+
+        pointList.add(gisPoint);
+    }
 }
