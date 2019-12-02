a3647cdc8e06b1aaec5339391799dace3ac6654a
Administrator <xx>
2019-12-03 00:16:24 +0800

    第6课,读取文件还有点问题

--- lesson6/src/main/java/org/walkgis/learngis/lesson6/Utils.java
+++ lesson6/src/main/java/org/walkgis/learngis/lesson6/Utils.java
@@ -31,12 +31,19 @@ public class Utils {
      * @return
      */
     public static byte[] int2Bytes(int num) {
-        byte[] result = new byte[4];
-        result[3] = (byte) (num >>> 24);//取最高8位放到0下标
-        result[2] = (byte) (num >>> 16);//取次高8为放到1下标
-        result[1] = (byte) (num >>> 8); //取次低8位放到2下标
-        result[0] = (byte) (num);      //取最低8位放到3下标
-        return result;
+//        byte[] result = new byte[4];
+//        result[3] = (byte) (num >>> 24);//取最高8位放到0下标
+//        result[2] = (byte) (num >>> 16);//取次高8为放到1下标
+//        result[1] = (byte) (num >>> 8); //取次低8位放到2下标
+//        result[0] = (byte) (num);      //取最低8位放到3下标
+//        return result;
+
+        byte[] bytes = new byte[4];
+        bytes[0] = (byte) (num & 0xff);
+        bytes[1] = (byte) ((num >> 8) & 0xff);
+        bytes[2] = (byte) ((num >> 16) & 0xff);
+        bytes[3] = (byte) ((num >> 24) & 0xff);
+        return bytes;
     }
 
     /**
@@ -46,19 +53,24 @@ public class Utils {
      * @return
      */
     public static int bytes2Int(byte[] bytes, int index) {
-        byte[] a = new byte[4];
-        int i = a.length - 1, j = bytes.length - index - 1;
-        for (; i >= 0; i--, j--) {//从b的尾部(即int值的低位)开始copy数据
-            if (j >= 0)
-                a[i] = bytes[index + j];
-            else
-                a[i] = 0;//如果b.length不足4,则将高位补0
-        }
-        int v0 = (a[3] & 0xff) << 24;//&0xff将byte值无差异转成int,避免Java自动类型提升后,会保留高位的符号位
-        int v1 = (a[2] & 0xff) << 16;
-        int v2 = (a[1] & 0xff) << 8;
-        int v3 = (a[0] & 0xff);
-        return v0 + v1 + v2 + v3;
+//        byte[] a = new byte[4];
+//        int i = a.length - 1, j = bytes.length - index - 1;
+//        for (; i >= 0; i--, j--) {//从b的尾部(即int值的低位)开始copy数据
+//            if (j >= 0)
+//                a[i] = bytes[index + j];
+//            else
+//                a[i] = 0;//如果b.length不足4,则将高位补0
+//        }
+//        int v0 = (a[3] & 0xff) << 24;//&0xff将byte值无差异转成int,避免Java自动类型提升后,会保留高位的符号位
+//        int v1 = (a[2] & 0xff) << 16;
+//        int v2 = (a[1] & 0xff) << 8;
+//        int v3 = (a[0] & 0xff);
+//        return v0 + v1 + v2 + v3;
+
+        return (bytes[index + 0] & 0xff)
+                | (bytes[index + 1] & 0xff) << 8
+                | (bytes[index + 2] & 0xff) << 16
+                | (bytes[index + 3] & 0xff) << 24;
     }
 
     /**
